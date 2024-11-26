f967348365e4f2b96321257ca4367e766bc5e333
fabiconcept <favourajokubi@gmail.com>
2024-11-26 14:54:40 +0100

    Update FooterSection: change footer layout, add designer credit, and reformat copyright text

--- app/sections/FooterSection.tsx
+++ app/sections/FooterSection.tsx
@@ -5,8 +5,8 @@ import { FaAt, FaPhone } from "react-icons/fa6";
 export default function FooterSection() {
     const todayYr: number = new Date().getFullYear();
     return (
-        <footer className="py-12 sm:px-24 px-8 bg-orange-400 text-white">
-            <section className="flex flex-wrap gap-6 justify-between">
+        <footer className="bg-orange-400 text-white">
+            <section className="py-12 sm:px-24 px-8 flex flex-wrap gap-6 justify-between">
                 <div>
                     <Image
                         src={"https://golden-gates.sirv.com/logo.png"}
@@ -25,7 +25,14 @@ export default function FooterSection() {
                     <span className="sm:ml-6 ml-3 flex gap-2 items-center"><FaPhone /> <Link href={"tel:+17706569593"}>+1 770 656 9593</Link></span>
                 </div>
             </section>
-            <p className="mt-8 text-center">© {todayYr} Goldwater all rights reserved</p>
+            <p className="mt-8 text-center flex items-center justify-between border-t sm:px-24 px-8 py-8 grided-white">
+                <span>
+                    Designed by: <Link href={"https//fabiconcept.online"} className="font-semibold hover:text-blue-700">Fabiconcept</Link>
+                </span>
+                <span>
+                    &copy; {todayYr} <span className="font-semibold">Goldwater</span> all rights reserved
+                </span>
+            </p>
 
         </footer>
     )
